6c0739f00050a248ffdf70e1b217925cebffd95e
wu <wu@wudeMacBook-Pro.lan>
2019-11-28 20:01:12 +0800

    add test page change style when open lock success

--- pages/test/test.js
+++ pages/test/test.js
@@ -1,12 +1,24 @@
 Page({
-  data: {},
+  data: {
+    message: '开锁中。。。',
+    hidden: true
+  },
   onLoad() {
+    var that = this;
     my.request({
       url: 'https://touchez.cn:8090/pyapi/openlock',
       dataType: 'json',
       success: function(res) {
         // my.alert({content: JSON.stringify(res)});
-        my.alert({content: '开锁成功'});
+        my.alert({
+          content: '开锁成功',
+          success: () => {
+            that.setData({
+              'hidden': false,
+              'message': '开锁成功'
+            });
+          }
+          });
       },
       fail: function(res) {
         my.alert({content: JSON.stringify(res)});
